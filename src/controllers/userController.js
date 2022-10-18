4e04829ab0b08ea9e874ae3a9b75d0d06959c04d
CongPhiNguyen <congphinguyen312@gmail.com>
2022-10-18 09:11:46 +0700

    api authen sign up

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,23 @@ const checkValidEmail = require("../helper/emailValid")
 const sendMail = require("../helper/sendMail")
 const crypto = require("crypto")
 const TemptUser = require("../models/temptUser.js")
+const User = require("../models/users.js")
+const bcrypt = require("bcrypt")
+
+const SALT_ROUND = 10
+
 class userController {
+  signUp = (req, res) => {
+    const { username, email, name, password } = req.body
+    User.create({
+      username: username,
+      password: bcrypt.hashSync(password, SALT_ROUND),
+      email: email,
+      fullName: name
+    })
+    res.status(200).send({ username, email, name, password })
+  }
+
   requestOTP = (req, res) => {
     // console.log("req.body", req.body)
     const { email, username } = req.body
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const userController = require("../controllers/userController")
 
 router.post("/request-otp", userController.requestOTP)
+router.post("/sign-up", userController.signUp)
 
 module.exports = router
