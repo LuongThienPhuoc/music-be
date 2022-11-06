3ff5ac6f2bf6f6afe58856bb326879121fbbefcf
CongPhiNguyen <congphinguyen312@gmail.com>
2022-11-06 19:32:01 +0700

    allow all cors

--- index.js
+++ index.js
@@ -11,15 +11,16 @@ const PORT = process.env.PORT || 5050
 
 app.use(bodyParser.json({ limit: 10000 }))
 app.use(bodyParser.urlencoded({ extended: true, limit: 10000 }))
+
+// app.head("/", cors(), (req, res) => {
+//   // console.info("HEAD /simple-cors");
+//   res.sendStatus(204)
+// })
+
 app.use(
   cors({
     credentials: true,
-    origin: [
-      "http://localhost:3000",
-      "https://p2tune.netlify.app/",
-      "*",
-      "https://p2tune.netlify.app"
-    ]
+    origin: true
   })
 )
 app.use(cookieParser())
@@ -29,4 +30,5 @@ route(app)
 
 app.listen(PORT, () => {
   console.log("Server is running at port " + PORT)
+  console.log("Re deploy at 6h44 6/11")
 })
