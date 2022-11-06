e181a32587c8a6255d178bbb51e4a4856073a723
CongPhiNguyen <congphinguyen312@gmail.com>
2022-11-06 13:10:42 +0700

    fix DB connection

--- src/config/configDb.js
+++ src/config/configDb.js
@@ -1,15 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 const PASSWORD_MONGODB = process.env.PASSWORD_MONGODB
-const URI = `mongodb+srv://taptap-totp:${PASSWORD_MONGODB}@cluster0.owcqs.mongodb.net/musicDatabase?retryWrites=true&w=majority`;
+  ? process.env.PASSWORD_MONGODB
+  : "mvWWPzdqNYJmjWfW"
+const URI = `mongodb+srv://taptap-totp:${PASSWORD_MONGODB}@cluster0.owcqs.mongodb.net/musicDatabase?retryWrites=true&w=majority`
 
 module.exports = connectDB = async () => {
   try {
     await mongoose.connect(URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connecting to DB successfully");
+      useUnifiedTopology: true
+    })
+    console.log("Connecting to DB successfully")
   } catch (e) {
-    console.log("ERROR", e);
+    console.log("ERROR", e)
   }
-};
+}
