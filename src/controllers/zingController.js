50bd866c33b64431815db97983e101d86a8e76a8
CongPhiNguyen <congphinguyen312@gmail.com>
2022-11-20 01:02:34 +0700

    fix some api

--- src/controllers/zingController.js
+++ src/controllers/zingController.js
@@ -2,7 +2,14 @@ const { ZingMp3 } = require("zingmp3-api-full")
 
 class zingController {
   getTop100 = async (req, res) => {
-    const data = await ZingMp3.getTop100()
+    const data = await ZingMp3.getChartHome()
+
+    res.status(200).send({ success: true, data: data })
+  }
+
+  getConcreteLyric = async (req, res) => {
+    const data = await ZingMp3.getSong("IWZ97FCD")
+
     res.status(200).send({ success: true, data: data })
   }
 }
--- src/routers/zing.js
+++ src/routers/zing.js
@@ -3,5 +3,5 @@ const router = express.Router()
 
 const zingController = require("../controllers/zingController")
 router.get("/get-top-100", zingController.getTop100)
-
+router.get("/get-concrete-lyric", zingController.getConcreteLyric)
 module.exports = router
