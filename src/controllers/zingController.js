e6adb87bd84abd083c69965b9f4d688280ad6dae
LuongThienPhuoc <thienphuoc19032001@gmail.com>
2022-12-04 10:22:42 +0700

    search api

--- src/controllers/zingController.js
+++ src/controllers/zingController.js
@@ -3,13 +3,36 @@ const { ZingMp3 } = require("zingmp3-api-full")
 class zingController {
   getTop100 = async (req, res) => {
     const data = await ZingMp3.getChartHome()
-
     res.status(200).send({ success: true, data: data })
+  }
+
+  getDetailSong = async (req, res) => {
+    const { idSong } = req.query
+    const detail = await ZingMp3.getSong(idSong)
+    res.status(200).json({ detail })
+  }
+
+  search = async (req, res) => {
+    const { search } = req.query
+    const data = await ZingMp3.search(search)
+    res.status(200).json({
+      data
+    })
+  }
+
+  getListMusic = async (req, res) => {
+    const musics = await ZingMp3.getListArtistSong()
+    const artists = await ZingMp3.getArtist("sontungmtp")
+    const search = await ZingMp3.getListArtistSong("IWZ9ZD8A", "1", "15")
+    const top100 = await ZingMp3.getTop100()
+    const home = await ZingMp3.getHome()
+    res.status(200).json({
+      home
+    })
   }
 
   getConcreteLyric = async (req, res) => {
     const data = await ZingMp3.getSong("IWZ97FCD")
-
     res.status(200).send({ success: true, data: data })
   }
 
@@ -25,11 +48,40 @@ class zingController {
       if (dataSong.err === 0) songFix.push(dataSong)
     }
     // console.log("songFix", songFix)
-    const data = await ZingMp3.getSong("IWZ97FCD")
+    const data = await ZingMp3.getSong("ZWFEOE7O")
     res
       .status(200)
       .send({ success: true, data: { data, zingChartID, songFix } })
   }
+
+  getRandomSongList = async (req, res) => {
+    const home = await ZingMp3.getHome()
+    const itemMain = home?.data?.items?.filter(
+      (val) => val.sectionType === "new-release"
+    )
+    const listSong = itemMain[0].items.all
+    // console.log(listSong.length)
+    // const getSongDetail = async (songIDs) => {
+    //   let songDetail
+    //   for (let i = 0; i < songIDs.length; i++) {
+    //     songDetail = await ZingMp3.getSong(songIDs[i])
+    //     if (songDetail.err == 0) {
+    //       break
+    //     }
+    //   }
+    //   return songDetail
+    // }
+    // let newList = []
+    // for (let i = 0; i < listSong.length; i++) {
+    //   let songConcrete = await getSongDetail(listSong[i].genreIds)
+    //   console.log("songConcrete", songConcrete)
+    //   if (songConcrete.err == 0) {
+    //     newList.push({ ...listSong[i], url: songConcrete.data[128] })
+    //   }
+    // }
+    res.status(200).send({ success: true, data: listSong })
+  }
+
 }
 
 module.exports = new zingController()
--- src/routers/zing.js
+++ src/routers/zing.js
@@ -2,8 +2,14 @@ const express = require("express")
 const router = express.Router()
 
 const zingController = require("../controllers/zingController")
+
+
+router.get("/get-random-song-list", zingController.getRandomSongList)
+router.get("/get-detail-song", zingController.getDetailSong)
+router.get("/search", zingController.search)
 router.get("/get-top-100", zingController.getTop100)
 router.get("/get-concrete-lyric", zingController.getConcreteLyric)
 router.get("/get-top-and-stream", zingController.getListLink)
+router.get("/get-list-music", zingController.getListMusic)
 
 module.exports = router
